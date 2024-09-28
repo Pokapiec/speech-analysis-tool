ff77e358afc5ca20dbdbbca45ded076546b2f3ff
p.okapiec <pawel@user.com>
2024-09-28 21:02:15 +0200

    add params stuff

--- analysis_tool/params.py
+++ analysis_tool/params.py
@@ -1,3 +1,17 @@
+from typing import TypedDict
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+class Envs:
+    OPENAPI_KEY: str
+
+
+def load_envs() -> Envs:
+    envs = {}
+    with open(f"{PROJECT_ROOT}/.env", "r") as f:
+        for line in f:
+            key, value = line.split("=", 1)
+            envs[key] = value
+    
+    return envs
